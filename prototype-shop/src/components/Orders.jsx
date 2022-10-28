ec8e40af20612faef39c27259bfce7e6019ce129
sanholl <edgar.jang93@hanmail.net>
2022-10-28 17:57:25 +0900

    prototype_shop layout 생성

---
+++ prototype-shop/src/components/Orders.jsx
@@ -0,0 +1,10 @@
+export default function Order() {
+  return (
+    <asdie>
+      <div className="empty">
+        <div className="title">You don't have any orders</div>
+        <div className="subtitle">Click on a + to add an order</div>
+      </div>
+    </asdie>
+  );
+}
